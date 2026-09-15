7530616d66352f6815d1085e0fb84f2646d55262
EMMA <JEmmanuelSS@outlook.com>
2026-09-14 19:50:16 -0600

    add ScreenShot every time a shot gets trigered

--- game.py
+++ game.py
@@ -1,14 +1,17 @@
 import vizdoom as vzd
-from vizdoom import Button
-from vizdoom import Mode
+from vizdoom import Button, Mode, ScreenFormat, ScreenResolution
 import time
+import cv2
+import numpy as np
+import os
 
 game = vzd.DoomGame()
 game.set_doom_map('E1M1')
 game.set_window_visible(True)
 game.set_config({
-    'screen_resolution' : vzd.ScreenResolution.RES_1400X1050
+    'screen_resolution' : ScreenResolution.RES_1400X1050
 })
+game.set_screen_format(ScreenFormat.BGR24)
 game.set_render_hud(True)
 game.set_available_buttons([
     Button.MOVE_FORWARD,
@@ -21,11 +24,35 @@ game.set_available_buttons([
     Button.USE
     ])
 game.set_mode(Mode.SPECTATOR)
+game.set_sound_enabled(True)
 
 game.init()
 
-while game.is_running():
+'''
+Captures Section:
+pwd : doom-mlg-ai/
+ls 
+/captures
+'''
+
+CAPTURES_DIR = "captures"
+photo_counter = 0
+
+while not game.is_episode_finished():
     game.advance_action()
-    time.sleep(.02)
+
+    if game.get_last_action()[4] == True:
+        
+        state = game.get_state()
+        frame = state.screen_buffer
+        photo_counter += 1
+
+        file_name = os.path.join(CAPTURES_DIR, f"doom_attack_{photo_counter}.jpg")
+
+        cv2.imwrite(file_name, frame)
+
+
+
+    time.sleep(.1)
 
 game.close()
